6d05a32523e43eaead220365ddbc44c22e19a05a
pritamdas972005-netizen <pritamdas972005@gamil.com>
2025-10-24 19:31:10 +0530

    successfully

---
+++ Function.js
@@ -0,0 +1,24 @@
+//write a function that prints your full name on the console.
+function printfullName(){
+    const fullname="Pritam Das";
+    console.log(fullname);
+}
+printfullName();
+//Create a function that takes two number prints their sum.
+function sum(){
+    const a=10;
+    const b=5;
+    const add=a+b;
+    console.log(add);
+}
+sum();
+//Create a function that accepts two numbers and returns the larger one.
+function larger(){
+    const a=10;
+    const b=20;
+    if(a<=b){
+       console.log("larger Number:",b);
+    }
+    
+}
+larger();
